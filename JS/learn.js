ee807d29639aa69f40c8ede16a7ec708d8844a60
puyhg5 <puyab500@gmail.com>
2017-06-09 12:13:00 +0200

    add js learn section

--- JS/learn.js
+++ JS/learn.js
@@ -1,10 +1,6 @@
+var gridBooks = document.querySelector("#grid-books");
 
-var titleHtml = document.querySelector("title-book");
-var authorHtml = document.querySelector("author-book");
-var valorationHtml = document.querySelector("user-valoration");
-var photoBookHtml = document.querySelector("cover-photo-book");
-
-function descargaArchivo() {
+function downloadFile() {
   // Obtener la instancia del objeto XMLHttpRequest
   if(window.XMLHttpRequest) {
      peticionHttp = new XMLHttpRequest();
@@ -28,24 +24,25 @@ function descargaArchivo() {
         var stopPoint = 9;
         console.log(stopPoint);
         //Recorremos todos los elementos Libro del documento
+        var booksHTML = "";
         for(var i = 0; i < stopPoint; i++){
-          libro = root.getElementsByTagName("best_book")[i];
-          console.log(libro);
-          autor = libro.getElementsByTagName("name")[0].firstChild.nodeValue;
-          console.log(autor);
-          titulo = libro.getElementsByTagName("title")[0].firstChild.nodeValue;
-          console.log(titulo);
-          img = libro.getElementsByTagName("image_url")[0].firstChild.nodeValue;
-          console.log(img);
-          // muestraHTML('pRespuesta',"Autor: "+autor+", t&iacute;tulo: "+titulo+"<br/>");
+          var book = root.getElementsByTagName("best_book")[i];
+          console.log(book);
+          var author = book.getElementsByTagName("name")[0].firstChild.nodeValue;
+          console.log(author);
+          var title = book.getElementsByTagName("title")[0].firstChild.nodeValue;
+          console.log(title);
+          var valoration = root.getElementsByTagName("average_rating")[0].firstChild.nodeValue;
+          console.log(title);
+          var cover = book.getElementsByTagName("image_url")[0].firstChild.nodeValue;
+          console.log(cover);
+          booksHTML += '<div class="random-book"><div class="cover-photo-book"><img class="img-cover-book" src="' + cover + '" alt=""></div><div class="info-book"><p class="title-book"><span class="bold-text">' + title + '</span></p><p class="author-book">' + author + '</p><p class="user-valoration">' + valoration + '/10</p><p class="isbn-number">ISBN:</p></div></div>';
         }
+        console.log(booksHTML);
+        gridBooks.innerHTML = booksHTML;
       }
     }
   }
-  function muestraHTML(id, texto){
-    if(document.getElementById){
-      document.getElementById(id).innerHTML += texto;
-    }
-  }
 }
-window.onload = descargaArchivo;
+
+window.onload = downloadFile;
