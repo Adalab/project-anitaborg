f4e84d934ab87c2b0a5bb4cd95ccc0c2e918c00d
puyhg5 <puyab500@gmail.com>
2017-06-21 10:22:16 +0200

    share in facebook

--- js/quiz.js
+++ js/quiz.js
@@ -9,22 +9,21 @@ var btnStart = document.querySelector('.btn-quiz');
 var btnNext = document.querySelector(".btn-next");
 var textQuiz = document.querySelector('.text-quiz');
 var scorePage = document.querySelector('.score-page');
-var fbRoot = document.querySelector('#fb-root');
 var fbButton = document.querySelector('.fb-button');
 var totalScore = document.querySelector('.score');
-var index = 0;
+var index=0;
 var info;
 var random;
 var result = 0;
 var request = new XMLHttpRequest();
-var value=0;
 
 function printTestQuestion() {
 
   if (index < 10) {
     if (index > 0 && random === parseInt(document.querySelector('.radio-format:checked').value)) {
       result = result + 1;
-    } else {
+    }
+    else{
       result = result + 0;
     }
     random = Math.floor(Math.random() * (4 - 0)) + 0;
@@ -34,25 +33,21 @@ function printTestQuestion() {
       positionAnswer.push(info.incorrectList[index][i]);
     }
     positionAnswer.splice(random, 0, info.correctList[index]);
-    number.innerHTML = 'Pregunta ' + (index + 1) + '/10';
-    question.innerHTML = info.questionList[index];
-    answerOne.innerHTML = positionAnswer[0];
-    answerTwo.innerHTML = positionAnswer[1];
-    answerThree.innerHTML = positionAnswer[2];
-    answerFour.innerHTML = positionAnswer[3];
-    index = index + 1;
-    ["option1", "option2", "option3", "option4"].forEach(function(id) {
-      document.getElementById(id).checked = false;
-    });
-    return false;
-  } else {
+    number.innerHTML='Pregunta '+(index +1)+'/10';
+    question.innerHTML=info.questionList[index];
+    answerOne.innerHTML=positionAnswer[0];
+    answerTwo.innerHTML=positionAnswer[1];
+    answerThree.innerHTML=positionAnswer[2];
+    answerFour.innerHTML=positionAnswer[3];
+    index=index+1;
+  }else{
     if (index > 0 && random === parseInt(document.querySelector('.radio-format:checked').value)) {
       result = result + 1;
-    } else {
+    }
+    else{
       result = result + 0;
     }
     scorePage.classList.remove('hidden');
-    fbRoot.classList.remove('hidden');
     fbButton.classList.remove('hidden');
     textQuiz.classList.add('hidden');
     btnNext.classList.add('hidden');
@@ -75,7 +70,7 @@ function getTestInfo() {
         info.correctList.push(data.results[i].correct_answer);
         info.incorrectList.push(data.results[i].incorrect_answers);
       }
-      printTestQuestion(info, index);
+      printTestQuestion(info,index);
       btnNext.addEventListener("click", printTestQuestion);
     } else {
       console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
@@ -88,10 +83,18 @@ function getTestInfo() {
   request.send();
 }
 
-function initTest() {
+function initTest(){
   getTestInfo();
   result = 0;
   index = 0;
 }
 
 btnStart.addEventListener('click', initTest);
+
+fbButton.onclick = function() {
+  FB.ui({
+    method: 'share',
+    mobile_iframe: true,
+    href: 'https://adalab.github.io/anitaBorg/',
+  }, function(response){});
+}
